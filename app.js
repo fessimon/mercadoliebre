89fb45a9acc22d494de9beca736d66d2550bbf94
fessimon <fessimon@gmail.com>
2022-12-26 18:43:55 -0300

    al 90

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 
 const app = express();
@@ -17,3 +17,30 @@ app.get('/', (req, res) => {
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, '/views/register.html'));
 });
+
+*/
+
+
+const express = require("express");
+const app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Servidor corriendo");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
+});
+
+
